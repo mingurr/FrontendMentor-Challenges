476f48e782c2f1823a541567baaf4a5edd95979a
Minkyung Kim <alsrud9138@gmail.com>
2026-03-20 21:33:57 +0900

    feat: finish tablet layout and JS interaction

--- article-preview-component-master/main.js
+++ article-preview-component-master/main.js
@@ -1,10 +1,19 @@
 const main = document.querySelector("main");
 const shareBox = document.querySelector(".share");
+const sharePopover = document.querySelector(".share-popover");
 
 main.addEventListener("click", () => {
-  if (shareBox.classList.contains("active")) {
-    shareBox.classList.remove("active");
-  } else {
-    shareBox.classList.add("active");
+  if (window.innerWidth > 377 && window.innerWidth < 768) {
+    if (shareBox.classList.contains("active")) {
+      shareBox.classList.remove("active");
+    } else {
+      shareBox.classList.add("active");
+    }
+  } else if (window.innerWidth >= 768 && window.innerWidth <= 1024) {
+    if (sharePopover.classList.contains("active")) {
+      sharePopover.classList.remove("active");
+    } else {
+      sharePopover.classList.add("active");
+    }
   }
 });
